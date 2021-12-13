1e1116e372d99526226174d06f591268d7a8406d
Augustine129 <74011126+Augustine129@users.noreply.github.com>
2021-12-13 17:11:03 +0000

    List of things I need to do

--- main.cs
+++ main.cs
@@ -2,6 +2,19 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    //List of things I need to do
+    // Provide instroctions of the game
+    //Greeting
+    //Displayer player 1 is X and Player 2 is of
+    //Dsplay which turn is it to play the game
+    //Display a game board
+    //the game will have 3 rows and 3 columns
+    //As the player places markers on the game  I need to update the table to notify which players has a turn
+    //After each turn judge there is a winner
+    // I there is no winner the player needs to keep playing
+    //If all markers are placed and there is no winner and its a draw the game should stop
+    // If we have a winner then the game should stop
   }
 }
+
+
