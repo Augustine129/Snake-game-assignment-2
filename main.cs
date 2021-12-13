b62f66a2addd93aba82134a95a011b80403febb8
Augustine129 <74011126+Augustine129@users.noreply.github.com>
2021-12-13 17:59:03 +0000

    Provide user output

--- main.cs
+++ main.cs
@@ -3,10 +3,20 @@ using System;
 class Program {
   public static void Main (string[] args) {
     //List of things I need to do
+
     // Provide instroctions of the game
     //Greeting
+    Console.WriteLine("Welcome to Augustine Tic Tac Toe Game !!");
+
     //Displayer player 1 is X and Player 2 is of
+    Console.WriteLine("Player 1: X");
+    Console.WriteLine("Player 2: O");
+
+
     //Dsplay which turn is it to play the game
+    //Tell the user to enter a number between 1 and 9
+    Console.WriteLine("Player {x} please select 1 throught 9 from the game board ");
+
     //Display a game board
     //the game will have 3 rows and 3 columns
     //As the player places markers on the game  I need to update the table to notify which players has a turn
